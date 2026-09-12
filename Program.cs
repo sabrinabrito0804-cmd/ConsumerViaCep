3fa4de466ec0aab0412f9d00d4163715ff15c9cf
Sabrina Custódio Brito <sabrinabrito0804@gmail.com>
2026-09-12 13:39:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsumerViaCep;
+using static System.Console;
+
+WriteLine("Digite o CEP que deseja consultar:");
+
+var cep = ReadLine();
+
+var enderecoUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+WriteLine($"Consultando o endereço na url: {enderecoUrl}");
+
+var cliente = new HttpClient();
+
+try
+{
+    HttpResponseMessage? response = await cliente.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    string respostaApi = await response.Content.ReadAsStringAsync();
+    
+    Endereco endereco = System.Text.Json.JsonSerializer.Deserialize<Endereco>(respostaApi);
+
+    WriteLine($"\nCEP: {endereco.Cep}");
+    WriteLine($"\nRua: {endereco.Logradouro}");
+    WriteLine($"\nComplemento: {endereco.Complemento}");
+    WriteLine($"\nBairro: {endereco.Bairro}");
+    WriteLine($"\nLocalidade: {endereco.Localidade}");
+
+
+    WriteLine($"\nResposta da API:\n{respostaApi}");
+}
+
+catch (Exception ex) 
+{
+WriteLine($"Ocorreu um erro ao consultar o endereço: {ex.Message}");
+}
